d7ae945104716be4718c8330c5729abb4b18857c
Lucas Axel Hess <lucashess98@gmail.com>
2022-08-12 00:29:48 -0300

    force to false

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const {
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(' listening at 3001'); // eslint-disable-line no-console
   });
