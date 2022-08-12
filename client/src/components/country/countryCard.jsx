51d616c48b15df725b8715c86b53d9c7d1d70a54
Lucas Axel Hess <lucashess98@gmail.com>
2022-08-11 23:16:06 -0300

    first commit pre-deploy

--- client/src/components/country/countryCard.jsx
+++ client/src/components/country/countryCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./countryCards.css";
 export default function CountryCard(props) {
   return (
