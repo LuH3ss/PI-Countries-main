ef140092e495ab794a630e2dc3ae2b4384ff4f04
Lucas Axel Hess <lucashess98@gmail.com>
2022-09-07 21:19:19 -0300

    comienzo de corrección

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, 
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`, 
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
